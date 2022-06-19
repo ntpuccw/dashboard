cb8f3b01f89383a90aaf9ad13db07418c9141e98
ntpuccw <ccw@gm.ntpu.edu.tw>
2022-06-19 12:23:26 +0800

    add plotly codes

--- streamlit_T_dist.py
+++ streamlit_T_dist.py
@@ -1,42 +1,52 @@
 # From "streamlit hello"
 
 import matplotlib.pyplot as plt
-from scipy.stats import t
+import plotly.express as px
+import plotly.graph_objects as go
+from scipy.stats import t, norm
 import streamlit as st
 import numpy as np
 import time
 
-# progress_bar = st.sidebar.progress(0)
-# status_text = st.sidebar.empty()
 df1 = np.linspace(0.1, 1, 10)
 df2 = np.arange(2, 31)
 df = np.hstack((df1, df2))
-# print(df)
+n = range(len(df))
+
+dof = st.slider('Degree of Freedom', min_value=n[0], max_value=n[-1], value=n[10], step=1)
+st.write("T(", df[dof],")")
 
 x = np.linspace(-5, 5, 1000)
 fig = plt.figure(figsize=(6,3))
+norm_y = norm.pdf(x) # Z
+plt.plot(x, norm_y, color = 'r', label = "Z")
+y = t.pdf(x, df[dof])
+plt.plot(x, y, color = 'b', alpha = 0.5, label = "T("+ str(df[dof])+")")
+plt.legend()
 
-for i in df:
-    y = t.pdf(x, i)
-    plt.plot(x, y, color = 'b', alpha = 0.5)
-    # status_text.text("%i%% degree of freedom" % i)
-    # time.sleep(0.05)
-    # plt.show()
-    plt.pause(0.5)
+st.pyplot(fig)
 
-# st.pyplot(fig)
-plt.show()
-# for i in df:
-#     new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-#     status_text.text("%i%% Complete" % i)
-#     chart.add_rows(new_rows)
-#     progress_bar.progress(i)
-#     last_rows = new_rows
-#     time.sleep(0.05)
+# fig2 = px.line(x = x, y = [norm_y, y])
+fig2 = px.line(x = x, y = y, title='T distribution')
+# st.write(fig2)
 
-# progress_bar.empty()
+st.plotly_chart(fig2)
 
-# Streamlit widgets automatically run the script from top to bottom. Since
-# this button is not connected to any other logic, it just causes a plain
-# rerun.
-# st.button("Re-run")
+
+d = st.slider('Slide to change the degree of freedom', \
+    min_value=n[0], max_value=n[-1], value=n[10], step=1, key='plotly')
+st.write("T(", df[d],")")
+
+y = t.pdf(x, df[d])
+
+fig3 = go.Figure()
+fig3.add_trace(go.Scatter(x=x, y=norm_y,
+                    mode='lines',
+                    name='Z',
+                    line=dict(color='firebrick', width=4)))
+fig3.add_trace(go.Scatter(x=x, y=y,
+                    mode='lines',
+                    name='T',
+                    line=dict(color='royalblue', width=4)))
+
+st.plotly_chart(fig3)
