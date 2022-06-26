47330c7b69e80ad080e4a882c9aa390f6e31a5d1
ntpuccw <ccw@gm.ntpu.edu.tw>
2022-06-26 12:19:32 +0800

    add search by chinese

--- espnaWord.py
+++ espnaWord.py
@@ -2,6 +2,8 @@ import sqlite3
 from sqlite3 import Error
 import pandas as pd
 import streamlit as st
+
+# from textblob import TextBlob
 # from googletrans import Translator
 # from google_trans_new import google_translator  
 # import googletrans
@@ -30,18 +32,48 @@ def searchByWord(conn, word):
     # df = pd.DataFrame(rows)
     # df.columns = names
     # return df    
-
+def searchByWord_zh(conn, word):
+    # sql = "select content from word_content where word='"+word+"'"
+    sql = "select word, content from word_content where content like'%"+word+"%'"
+    # print(sql)
+    cur = conn.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+    return rows
 # def word_color(url):
     #  st.markdown(f'<p style="background-color:#0066cc;color:#33ff33;font-size:20px;border-radius:2%;">{url}</p>', unsafe_allow_html=True)
     #  st.markdown(f'<p style="color:#ffff00;font-size:20px;">{url}</p>', unsafe_allow_html=True)
 def main():
-    st.title("稀罕·西漢辭典")
+    st.title("稀罕·西漢漢西辭典")
     database = r"espnaDict.sqlite"
     conn = create_connection(database)
     
     word = st.text_input('輸入西班牙語單字查詢中文辭義（完整單字或前部分字母皆可）', 'españ')
+    
+    # print(TextBlob(word).detect_language())
+    # translator = google_translator()
+    # dt1 = translator.detect(word)
+    # print(dt1)
+
     st.write('---')
     df = searchByWord(conn, word)
+    if len(df) ==0:
+        st.write('~ Nothing Found ~')
+    else:
+    # need to separate linebreak by \n
+        for j in range(len(df)):
+            col1, col2 = st.columns((1,2))
+            tmp = df[j][1].split(sep='\n') 
+            with col1:
+                st.write(f"<p style='color:#FF4500'>{df[j][0]}</p>", unsafe_allow_html=True)
+            for line in tmp:
+                with col2:
+                    st.write(line)
+                    
+    st.write('---')
+    word_zh = st.text_input('輸入中文字查詢相關西文單字', '番紅花')
+    st.write('---')
+    df = searchByWord_zh(conn, word_zh)
     if len(df) ==0:
         st.write('~ Nothing Found ~')
     else:
