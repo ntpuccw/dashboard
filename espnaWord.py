44df31c5afc0bf0e29ab33088ab8ed78cd15ed11
ntpuccw <ccw@gm.ntpu.edu.tw>
2022-06-26 12:27:04 +0800

    --

--- espnaWord.py
+++ espnaWord.py
@@ -44,7 +44,7 @@ def searchByWord_zh(conn, word):
     #  st.markdown(f'<p style="background-color:#0066cc;color:#33ff33;font-size:20px;border-radius:2%;">{url}</p>', unsafe_allow_html=True)
     #  st.markdown(f'<p style="color:#ffff00;font-size:20px;">{url}</p>', unsafe_allow_html=True)
 def main():
-    st.title("稀罕·西漢漢西辭典")
+    st.title("稀罕·西漢/漢西辭典")
     database = r"espnaDict.sqlite"
     conn = create_connection(database)
     
